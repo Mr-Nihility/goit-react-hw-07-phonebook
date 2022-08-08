f2fcba0e87fad5f6c741673541e6cc62a9b646c6
Volodymyr Romanenko <98472440+Mr-Nihility@users.noreply.github.com>
2022-08-08 11:27:49 +0300

    Update contacts-reducers.js

--- src/redux/contacts/contacts-reducers.js
+++ src/redux/contacts/contacts-reducers.js
@@ -2,14 +2,8 @@ import { addUser, deleteUser, filterUser } from './contscts-actions';
 import { createReducer } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 //----------------------------------------------------------------//
-const initContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
 
-const itemReducer = createReducer(initContacts, {
+const itemReducer = createReducer([], {
   [addUser.type]: (state, { payload }) => [...state, payload],
   [deleteUser.type]: (state, { payload }) =>
     state.filter(({ id }) => id !== payload),
